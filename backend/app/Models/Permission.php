b0325fc54ad2983c3e98ffad3ac242158ea7a29c
ahmedsaliuGit <saliu.lito@gmail.com>
2026-06-25 19:01:19 +0100

    model(permission_many_role): Implement permission and role relationship

--- backend/app/Models/Permission.php
+++ backend/app/Models/Permission.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $guarded = [
+        'id',
+        'name',
+        'slug',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
